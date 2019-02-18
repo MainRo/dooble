02f5d0c68dfb34af037c77ca122f62d6bded6cf0
Romain Picard <romain.picard@oakbits.com>
2019-02-18 23:48:38 +0100

    Bump version: 0.4.0 → 0.5.0

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/mainro/dooble',
-    version='0.4.0',
+    version='0.5.0',
     zip_safe=False,
     entry_points={
         'console_scripts': ['dooble=dooble.cli:main'],
